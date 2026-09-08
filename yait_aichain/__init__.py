ea66cecb2330892800951d9678e38dd211414c07
yaitio <hello@yait.io>
2026-09-08 22:43:08 +0300

    release 2.2.0

--- yait_aichain/__init__.py
+++ yait_aichain/__init__.py
@@ -27,7 +27,7 @@ Full imports::
     from yait_aichain.tools  import convertToMD, searchPerplexity, Embedding
 """
 
-__version__ = "2.1.0"
+__version__ = "2.2.0"
 __author__  = "YAIT"
 
 # ── Logging ────────────────────────────────────────────────────────────────────
